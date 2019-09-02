ac72e064bf712c43e0f4d0995d0b1d26cb180f53
PedroPierina <PedroPierina@users.noreply.github.com>
2019-09-02 08:00:20 -0300

    Add files via upload

--- src/Window.java
+++ src/Window.java
@@ -3,6 +3,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 import javax.swing.Box;
 import javax.swing.JList;
@@ -21,6 +22,7 @@ import javax.swing.JComponent;
 
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
+import java.util.Vector;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -51,7 +53,6 @@ public class Window extends JFrame {
 			}
 		});
 	}
-	
 
 	/**
 	 * Create the frame.
@@ -65,128 +66,138 @@ public class Window extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JLabel lblInstrues = new JLabel("Instru\u00E7\u00F5es");
-		lblInstrues.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblInstrues.setHorizontalAlignment(SwingConstants.CENTER);
-		lblInstrues.setLabelFor(this);
-		lblInstrues.setBounds(10, 0, 400, 25);
-		contentPane.add(lblInstrues);
-		
-		DefaultTableModel model = new DefaultTableModel();
-		JTable instructionsTable = new JTable(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"Linha", "Instrução", "Atributo 1", "Atributo 2", "Comentário"
-			}
-		) {
-			Class[] columnTypes = new Class[] {
-				Integer.class, String.class, Integer.class, Integer.class, String.class
-			};
+
+//		DefaultTableModel model = new DefaultTableModel();
+//		JTable instructionsTable = new JTable(model);
+//		instructionsTable.setBorder(new LineBorder(new Color(0, 0, 0)));
+//		instructionsTable.setBounds(10, 22, 446, 323);
+//
+//		instructionsTable.setModel(new DefaultTableModel(new Object[][] {
+//
+//		}, new String[] { "Instrucao", "Parametro1", "Parametro2" }));
+//		
+//		Vector row = new Vector();
+//		row.add("Instrucao1");
+//		row.add("Para1");
+//		row.add("Para2");
+//		model.addRow(row);
+//		contentPane.add(instructionsTable);
+
+		JScrollPane scroll = new JScrollPane();
+//		DefaultTableModel model = new DefaultTableModel();
+
+//		TableColumn coluna1 = new TableColumn();
+//		model.addColumn("Instrução");
+//		model.addColumn("Parametro 1");
+//		model.addColumn("Parametro 2");
+
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(10, 22, 446, 318);
+		contentPane.add(scrollPane_1);
+		JTable instructionsTable_1 = new JTable(new DefaultTableModel(new Object[][] {},
+				new String[] { "Linha", "Instrução", "Atributo 1", "Atributo 2", "Comentário" }) {
+			Class[] columnTypes = new Class[] { Integer.class, String.class, Integer.class, Integer.class,
+					String.class };
+
 			public Class getColumnClass(int columnIndex) {
 				return columnTypes[columnIndex];
 			}
 		});
-		instructionsTable.setBorder(new LineBorder(new Color(0, 0, 0)));
-		instructionsTable.setBounds(10, 22, 446, 162);
-		
-		TableColumn coluna1 = new TableColumn();
-		model.addColumn("Instrução");
-		model.addColumn("Parametro 1");
-		model.addColumn("Parametro 2");
-		instructionsTable.addColumn(coluna1);
-		contentPane.add(instructionsTable);
-		
+		scrollPane_1.setViewportView(instructionsTable_1);
+//		instructionsTable_1.addColumn(coluna1);
+		instructionsTable_1.setGridColor(Color.BLACK);
+		instructionsTable_1.setShowGrid(true);
+
 		JLabel lblPilha = new JLabel("Pilha");
 		lblPilha.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblPilha.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPilha.setBounds(452, -3, 187, 30);
 		contentPane.add(lblPilha);
-		
+
 		pilhaTable = new JTable();
 		pilhaTable.setBorder(new LineBorder(new Color(0, 0, 0)));
 		pilhaTable.setBounds(466, 22, 163, 501);
 		contentPane.add(pilhaTable);
-		
+
 		JLabel lblEntrada = new JLabel("Entrada");
 		lblEntrada.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblEntrada.setHorizontalAlignment(SwingConstants.CENTER);
 		lblEntrada.setBounds(21, 368, 129, 25);
 		contentPane.add(lblEntrada);
-		
+
 		JTextArea inputArea = new JTextArea();
 		inputArea.setBounds(10, 392, 154, 110);
 		contentPane.add(inputArea);
-		
+
 		inputField = new JTextField();
 		inputField.setBounds(10, 499, 158, 25);
 		contentPane.add(inputField);
 		inputField.setColumns(1);
-		
+
 		JLabel lblSada = new JLabel("Sa\u00EDda");
 		lblSada.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblSada.setHorizontalAlignment(SwingConstants.CENTER);
 		lblSada.setBounds(203, 368, 129, 25);
 		contentPane.add(lblSada);
-		
+
 		JTextArea outputArea = new JTextArea();
 		outputArea.setBounds(188, 392, 163, 131);
 		contentPane.add(outputArea);
-		
+
 		JLabel lblBreakPoint = new JLabel("Break Point");
 		lblBreakPoint.setFont(new Font("Tahoma", Font.BOLD, 11));
 		lblBreakPoint.setHorizontalAlignment(SwingConstants.CENTER);
 		lblBreakPoint.setBounds(361, 368, 99, 25);
 		contentPane.add(lblBreakPoint);
-		
+
 		JButton jumpButton = new JButton("Jump");
 		jumpButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		
+
 		JTextArea breakArea = new JTextArea();
 		breakArea.setBounds(361, 392, 99, 110);
 		contentPane.add(breakArea);
-		
+
 		breakField = new JTextField();
 		breakField.setBounds(361, 498, 99, 28);
 		contentPane.add(breakField);
 		breakField.setColumns(10);
 		jumpButton.setBounds(350, 537, 89, 23);
 		contentPane.add(jumpButton);
-		
+
 		JButton continueButton = new JButton("Continue");
 		continueButton.setBounds(203, 537, 99, 23);
 		contentPane.add(continueButton);
 
-		
 		JButton btnStart = new JButton("Start");
 		btnStart.setBounds(6, 535, 75, 29);
 		contentPane.add(btnStart);
-		
-		
+
 		JButton btnStop = new JButton("Stop");
 		btnStop.setBounds(93, 534, 75, 29);
 		contentPane.add(btnStop);
-		
-		InstructionList instructions = new InstructionList("D:\\Workspace\\VirtualMachine-dev\\VirtualMachine\\object.txt");
+
+		JLabel lblInstrues = new JLabel("Instru\u00E7\u00F5es");
+		lblInstrues.setBounds(0, 0, 396, 21);
+		contentPane.add(lblInstrues);
+		lblInstrues.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblInstrues.setHorizontalAlignment(SwingConstants.CENTER);
+		lblInstrues.setLabelFor(this);
+
+//		InstructionList instructions = new InstructionList("D:\\Workspace\\VirtualMachine-dev\\VirtualMachine\\object.txt");
+		InstructionList instructions = new InstructionList("‎⁨/Users/pedro/Desktop/object.txt");
 		for (Instruction list : instructions.list) {
-//			System.out.println(list.instructionName + " " + list.argument1 + " " + list.argument2);
+			int count = 1;
+			System.out.println(list.instructionName + " " + list.argument1 + " " + list.argument2);
+			DefaultTableModel model = (DefaultTableModel) instructionsTable_1.getModel();
+			model.addRow(new Object[]{count, list.instructionName, list.argument1, list.argument2, "NUll"});
+			count++;
 //			list.readInstruction();
+			
 		}
-		
-		
-		JTextPane txtpnTeste = new JTextPane();
-		txtpnTeste.setText("");
-		txtpnTeste.setBounds(20, 196, 437, 139);
-		contentPane.add(txtpnTeste);
-		
+
 		String aux = new String();
-		
-		txtpnTeste.setText("Oba1");
-		aux = txtpnTeste.getText();
-		txtpnTeste.setText(aux + "\n" + "Oba2");
 	}
 }
